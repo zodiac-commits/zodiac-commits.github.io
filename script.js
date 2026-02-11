e5a473ca3dec3c5d218de3bc6cf6d7ee9564a05f
zodiac-commits <jaketang1007@gmail.com>
2026-02-10 20:42:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ noButton.addEventListener("click", function () {
 });
 
 // Yes 按钮点击后，进入表白成功页面
-const loveTest = `!!!喜欢你!! ( >᎑<)♡︎ᐝ  ${
+const loveTest = `!!!I Love You!! ( >᎑<)♡︎ᐝ  ${
   username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
 }`;
 
@@ -77,3 +77,4 @@ yesButton.addEventListener("click", function () {
   document.body.style.overflow = "hidden";
 
 });
+
